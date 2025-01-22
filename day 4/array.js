6a663637342632e024528473f7083d4fa2a94f90
preethi-2535 <preethisaibathini>
2025-01-22 19:02:31 +0530

    pushed

---
+++ day 4/array.js
@@ -0,0 +1,14 @@
+// console.log(10);
+// console.log(20);
+// // setTimeout(()=>{
+// //     console.log(30);
+// // } , 5000)
+// setInterval(()=>{
+//     // console.log("setInterval");
+//     document.writeln("MRU");
+// },1500)
+// console.log(40);
+// console.log(50);
+
+
+//! Promise
